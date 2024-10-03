9e1a4b143d5cd6f2cb5e7dc4902ebc52c4017a3f
isamuella <s.ineza@alustudent.com>
2024-10-03 17:33:56 +0200

    workdone

--- create_environment.sh
+++ create_environment.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-mkdir -p submission_reminder_app
+mkdir -p submission_reminder_app/{app,modules,assets,config}
 
 cd submission_reminder_app
 
-cat
+cat /c/Users/Samuella/Documents/reminder.sh > app/reminder.sh
+cat /c/Users/Samuella/Documents/functions.sh > modules/functions.sh
+cat /c/Users/Samuella/Documents/config.env > config/config.env
+cat ../submissions.txt > assets/submissions.txt
+
+# five more students
+echo -e "Larrisa, Shell Navigation, submitted\nTonny, Shell Navigation, not submitted\nBienvenu, Shell Navigation, submitted\nGloria, Shell Navigation, submitted\nSam, Shell Navigation, not submitted" >> assets/submissions.txt
+
+echo -e "#!/bin/bash\n./app/reminder.sh" >> startup.sh
